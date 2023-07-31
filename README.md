c540c4a68c986c78caa330d3b14fefeb1819e638
mrdrivingduck <mrdrivingduck@gmail.com>
2023-07-31 09:59:05 +0800

    refactor: Dockerfiles and README

---
+++ README.md
@@ -0,0 +1,97 @@
+# PolarDB-PG Images
+
+## Runtime/Development Image
+
+[`polardb/polardb_pg_devel`](https://hub.docker.com/r/polardb/polardb_pg_devel/tags) provides runtime environment and compilation dependencies for PolarDB-PG. According to the base OS being used, it includes following tags:
+
+- `ubuntu20.04` (`latest`): use [`ubuntu:20.04`](https://hub.docker.com/_/ubuntu/tags) as base OS
+- `centos7` (DEPRECATED): use [`centos:centos7`](https://hub.docker.com/_/centos/tags) as base OS
+
+## Binary Image
+
+[`polardb/polardb_pg_binary`](https://hub.docker.com/r/polardb/polardb_pg_binary/tags) is based on `polardb/polardb_pg_devel:latest`, providing latest binary built from stable branch of PolarDB-PG. This image is enough for running PolarDB-PG. According to the storage type used by PolarDB-PG, it includes following tags:
+
+- `localfs`: the PolarDB-PG binary is compiled with local file system, so it can use local disk as its storage
+- `pfs`: the PolarDB-PG binary is compiled with PolarDB File System (PFS) SDK, so it can use shared disk as its storage
+
+## Local Instance Image
+
+[`polardb/polardb_pg_local_instance`](https://hub.docker.com/r/polardb/polardb_pg_local_instance/tags) is based on `polardb/polardb_pg_binary:localfs`, with an entrypoint for initializing and starting-up PolarDB-PG instance on local file system.
+
+### Without Data Persistence
+
+Simply try without persisting data directories. It will initialize database inside the container and start the database up. Override Docker `CMD` of image with `psql` to connect to the database:
+
+```shell
+$ docker run -it --rm polardb/polardb_pg_local_instance psql
+...
+psql (11.9)
+Type "help" for help.
+
+postgres=#
+```
+
+If you don't want to get into `psql`, simply override Docker `CMD` with other command:
+
+```shell
+$ docker run -it --rm \
+    polardb/polardb_pg_local_instance \
+    bash
+...
+postgres@f9b569fed05b:~$
+```
+
+### With Data Persistence
+
+Use an empty local directory as a volume, and mount the volume when creating the container. The container entry point will try to initdb in this volume. `${your_data_dir}` should be empty for the first time. If the volume is not emtpy, the entry point will regard the volume as already been initialized, and will start-up the database instance from the volume. The volume must be mounted under `/var/polardb/` inside container:
+
+```shell
+docker run -it --rm \
+    -v ${your_data_dir}:/var/polardb \
+    polardb/polardb_pg_local_instance \
+    psql
+```
+
+### Running as Daemon
+
+For those who want to use PolarDB-PG as a daemon service, the container can be created with `-d` option:
+
+```shell
+docker run -d \
+    -v ${your_data_dir}:/var/polardb \
+    polardb/polardb_pg_local_instance
+```
+
+### Exposing Ports
+
+For those who want to export ports to host machine, use `POLARDB_PORT` environment variable to specify ports. Typically, PolarDB-PG needs three continuous unused ports. Use `-p` option to expose these ports, e.g. `5432-5434`:
+
+```shell
+docker run -d \
+    --env POLARDB_PORT=5432 \
+    -p 5432-5434:5432-5434 \
+    -v ${your_data_dir}:/var/polardb \
+    polardb/polardb_pg_local_instance
+```
+
+Or directly use `--network=host` to share network with host machine:
+
+```shell
+$ docker run -d \
+    --network=host \
+    --env POLARDB_PORT=5432 \
+    -v ${your_data_dir}:/var/polardb \
+    polardb/polardb_pg_local_instance
+$ ...
+$ psql -h 127.0.0.1 -p 5432 -U postgres
+psql (11.9)
+Type "help" for help.
+
+postgres=#
+```
+
+### Environment Variables
+
+- `POLARDB_PORT`: the port on which primary node will be running; two replica nodes will be running at `${POLARDB_PORT}+1` and `${POLARDB_PORT}+2`
+- `POLARDB_USER`: the default superuser to be created during initialization
+- `POLARDB_PASSWORD`: the default password to be used by `POLARDB_USER` or `postgres`
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -9,52 +9,60 @@ primary_datadir="$polar_data_dir/primary_datadir"
 replica_datadir="$polar_data_dir/replica_datadir"
 shared_datadir="$polar_data_dir/shared_datadir"
 
-polardb_init() {
-    repnum=2
-    primary_port=$PGPORT
+repnum=2
 
+polardb_init() {
+    primary_port=${POLARDB_PORT:-5432}
+
+    # primary private dir and shared dir
     rm -rf ${primary_datadir}
     rm -rf ${shared_datadir}
     mkdir -p ${primary_datadir}
     mkdir -p ${shared_datadir}
 
+    # initdb
     initdb -k -U postgres -D ${primary_datadir}
 
-    echo "polar_enable_shared_storage_mode = on
-    polar_hostid = 1
-    max_connections = 100
-    polar_wal_pipeline_enable = true
-    polar_create_table_with_full_replica_identity = off
-    logging_collector = on
-    log_directory = 'pg_log'
+    # default GUCs
+    echo "polar_enable_shared_storage_mode = on" >> ${primary_datadir}/postgresql.conf
+    echo "polar_hostid = 1" >> ${primary_datadir}/postgresql.conf
+    echo "max_connections = 100" >> ${primary_datadir}/postgresql.conf
+    echo "polar_wal_pipeline_enable = true" >> ${primary_datadir}/postgresql.conf
+    echo "polar_create_table_with_full_replica_identity = off" >> ${primary_datadir}/postgresql.conf
+    echo "logging_collector = on" >> ${primary_datadir}/postgresql.conf
+    echo "log_directory = 'pg_log'" >> ${primary_datadir}/postgresql.conf
 
-    unix_socket_directories='.'
-    shared_buffers = 128MB
-    synchronous_commit = on
-    full_page_writes = off
-    #random_page_cost = 1.1
-    autovacuum_naptime = 10min
-    max_worker_processes = 32
-    polar_use_statistical_relpages = off
-    polar_enable_persisted_buffer_pool = off
-    polar_nblocks_cache_mode = 'all'
-    polar_enable_replica_use_smgr_cache = on
-    polar_enable_standby_use_smgr_cache = on
-    polar_enable_flashback_log = on" >> ${primary_datadir}/postgresql.conf
+    echo "shared_buffers = 128MB" >> ${primary_datadir}/postgresql.conf
+    echo "synchronous_commit = on" >> ${primary_datadir}/postgresql.conf
+    echo "full_page_writes = off" >> ${primary_datadir}/postgresql.conf
+    echo "autovacuum_naptime = 10min" >> ${primary_datadir}/postgresql.conf
+    echo "max_worker_processes = 32" >> ${primary_datadir}/postgresql.conf
+    echo "polar_use_statistical_relpages = off" >> ${primary_datadir}/postgresql.conf
+    echo "polar_enable_persisted_buffer_pool = off" >> ${primary_datadir}/postgresql.conf
+    echo "polar_nblocks_cache_mode = 'all'" >> ${primary_datadir}/postgresql.conf
+    echo "polar_enable_replica_use_smgr_cache = on" >> ${primary_datadir}/postgresql.conf
+    echo "polar_enable_standby_use_smgr_cache = on" >> ${primary_datadir}/postgresql.conf
+    echo "polar_enable_flashback_log = on" >> ${primary_datadir}/postgresql.conf
 
+    # storage-related GUCs
     disk_name=`echo ${shared_datadir} | cut -d '/' -f2`
-    echo "polar_vfs.localfs_mode = true
-    polar_enable_localfs_test_mode = on
-    polar_enable_shared_storage_mode = on
-    listen_addresses = '*'
-    polar_disk_name = '$disk_name'
-    polar_datadir = 'file-dio://$shared_datadir'" >> ${primary_datadir}/postgresql.conf
+    echo "polar_vfs.localfs_mode = true" >> ${primary_datadir}/postgresql.conf
+    echo "polar_enable_localfs_test_mode = on" >> ${primary_datadir}/postgresql.conf
+    echo "polar_enable_shared_storage_mode = on" >> ${primary_datadir}/postgresql.conf
+    echo "listen_addresses = '*'" >> ${primary_datadir}/postgresql.conf
+    echo "polar_disk_name = '$disk_name'" >> ${primary_datadir}/postgresql.conf
+    echo "polar_datadir = 'file-dio://$shared_datadir'" >> ${primary_datadir}/postgresql.conf
 
-    echo "shared_preload_libraries = '\$libdir/polar_px,\$libdir/polar_vfs,\$libdir/polar_worker,\$libdir/pg_stat_statements,\$libdir/auth_delay,\$libdir/auto_explain,\$libdir/polar_monitor_preload,\$libdir/polar_stat_sql'" >> ${primary_datadir}/postgresql.conf && \
+    # preload extensions
+    echo "shared_preload_libraries = '\$libdir/polar_px,\$libdir/polar_vfs,\$libdir/polar_worker,\$libdir/pg_stat_statements,\$libdir/auth_delay,\$libdir/auto_explain,\$libdir/polar_monitor_preload,\$libdir/polar_stat_sql'" >> ${primary_datadir}/postgresql.conf
+
+    # shared dir initialization
     polar-initdb.sh ${primary_datadir}/ ${shared_datadir}/ localfs
 
+    # allow external connections
     echo "host all all 0.0.0.0/0 md5" >> ${primary_datadir}/pg_hba.conf
 
+    # replica initialization
     for i in $(seq 1 $repnum)
     do
         dir=${replica_datadir}${i}
@@ -72,42 +80,63 @@ polardb_init() {
         echo "recovery_target_timeline = 'latest'" >> $dir/recovery.conf
     done
 
-    echo "port = $primary_port
-        polar_hostid = 100
-        full_page_writes = off" >> $primary_datadir/postgresql.conf
+    echo "port = $primary_port" >> ${primary_datadir}/postgresql.conf
+    echo "polar_hostid = 100" >> ${primary_datadir}/postgresql.conf
+    echo "full_page_writes = off" >> ${primary_datadir}/postgresql.conf
 
-    echo "polar_enable_px=0" >> $primary_datadir/postgresql.conf
-    echo "polar_px_enable_check_workers=0" >> $primary_datadir/postgresql.conf
-    echo "polar_px_enable_replay_wait=1" >> $primary_datadir/postgresql.conf
-    echo "polar_px_dop_per_node=3" >> $primary_datadir/postgresql.conf
-    echo "polar_px_max_workers_number=0" >> $primary_datadir/postgresql.conf
-    echo "polar_px_enable_cte_shared_scan=1" >> $primary_datadir/postgresql.conf
-    echo "polar_px_enable_partition=1" >> $primary_datadir/postgresql.conf
-    echo "polar_px_enable_left_index_nestloop_join=1" >> $primary_datadir/postgresql.conf
-    echo "polar_px_wait_lock_timeout=1800000" >> $primary_datadir/postgresql.conf
-    echo "polar_px_enable_partitionwise_join=1" >> $primary_datadir/postgresql.conf
-    echo "polar_px_optimizer_multilevel_partitioning=1" >> $primary_datadir/postgresql.conf
-    echo "polar_px_max_slices=1000000" >> $primary_datadir/postgresql.conf
-    echo "polar_px_enable_adps=1" >> $primary_datadir/postgresql.conf
-    echo "polar_px_enable_adps_explain_analyze=1" >> $primary_datadir/postgresql.conf
-    echo "polar_trace_heap_scan_flow=1" >> $primary_datadir/postgresql.conf
-    echo "polar_px_enable_spi_read_all_namespaces=1" >> $primary_datadir/postgresql.conf
+    # PX related GUCs
+    echo "polar_enable_px=0" >> ${primary_datadir}/postgresql.conf
+    echo "polar_px_enable_check_workers=0" >> ${primary_datadir}/postgresql.conf
+    echo "polar_px_enable_replay_wait=1" >> ${primary_datadir}/postgresql.conf
+    echo "polar_px_dop_per_node=3" >> ${primary_datadir}/postgresql.conf
+    echo "polar_px_max_workers_number=0" >> ${primary_datadir}/postgresql.conf
+    echo "polar_px_enable_cte_shared_scan=1" >> ${primary_datadir}/postgresql.conf
+    echo "polar_px_enable_partition=1" >> ${primary_datadir}/postgresql.conf
+    echo "polar_px_enable_left_index_nestloop_join=1" >> ${primary_datadir}/postgresql.conf
+    echo "polar_px_wait_lock_timeout=1800000" >> ${primary_datadir}/postgresql.conf
+    echo "polar_px_enable_partitionwise_join=1" >> ${primary_datadir}/postgresql.conf
+    echo "polar_px_optimizer_multilevel_partitioning=1" >> ${primary_datadir}/postgresql.conf
+    echo "polar_px_max_slices=1000000" >> ${primary_datadir}/postgresql.conf
+    echo "polar_px_enable_adps=1" >> ${primary_datadir}/postgresql.conf
+    echo "polar_px_enable_adps_explain_analyze=1" >> ${primary_datadir}/postgresql.conf
+    echo "polar_trace_heap_scan_flow=1" >> ${primary_datadir}/postgresql.conf
+    echo "polar_px_enable_spi_read_all_namespaces=1" >> ${primary_datadir}/postgresql.conf
 
+    # start up primary node
     pg_ctl -D ${primary_datadir} start
+
+    # create replication slots
     for i in $(seq 1 $repnum)
     do
-        psql -h 127.0.0.1 -d postgres -c "SELECT * FROM pg_create_physical_replication_slot('replica${i}')"
+        psql -h 127.0.0.1 -p $primary_port -d postgres -c "SELECT * FROM pg_create_physical_replication_slot('replica${i}')"
     done
+
+    # create default user with password, if specified
+    if [[ ${POLARDB_USER} == "postgres" ]];
+    then
+        if [[ -n ${POLARDB_PASSWORD} ]];
+        then
+            psql -h 127.0.0.1 -p $primary_port -d postgres -c "ALTER ROLE ${POLARDB_USER} PASSWORD '${POLARDB_PASSWORD}'"
+        fi
+    elif [[ -n ${POLARDB_USER} ]];
+    then
+        if [[ -n ${POLARDB_PASSWORD} ]];
+        then
+            psql -h 127.0.0.1 -p $primary_port -d postgres -c "CREATE ROLE ${POLARDB_USER} PASSWORD '${POLARDB_PASSWORD}' SUPERUSER LOGIN"
+        fi
+    fi
+
+    # stop primary node
     pg_ctl -D ${primary_datadir} stop
 }
 
-# If the data dir in empty, we will try to initdb here.
+# If the data volume is empty, we will try to initdb here.
 if [ -z "$(ls -A $polar_data_dir)" ];
 then
    polardb_init
 fi
 
-# Start the database.
+# Start-up PolarDB-PG.
 pg_ctl -D ${primary_datadir} start
 for i in $(seq 1 $repnum)
 do
@@ -123,3 +152,18 @@ then
 else
     eval ${@:1}
 fi
+
+# Stop PolarDB-PG.
+if [ ! $(pg_ctl -D ${primary_datadir} status | grep -q "server is running") ];
+then
+    pg_ctl -D ${primary_datadir} stop
+fi
+
+for i in $(seq 1 $repnum)
+do
+    dir=${replica_datadir}${i}
+    if [ ! $(pg_ctl -D ${dir} status | grep -q "server is running") ];
+    then
+        pg_ctl -D ${dir} stop
+    fi
+done
